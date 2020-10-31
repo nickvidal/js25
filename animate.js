798b89167ec67287b8319a8b52b4d7a8daa4a501
Nick Vidal <nick@opensource.org>
2020-10-31 16:32:17 -0300

    Create animate.js

---
+++ animate.js
@@ -0,0 +1,16 @@
+
+const milestones = document.querySelectorAll('.timeline-item');
+
+observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.classList.add('animate');
+    } else {
+      entry.target.classList.remove('animate');
+    }
+  });
+});
+
+milestones.forEach(milestone => {
+  observer.observe(milestone);
+});
